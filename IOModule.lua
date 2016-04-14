b06f74599754f32ad9e7de5b943a19c3c3a7d8d7
Gabriel Rossi <rrossigabriel@gmail.com>
2016-04-13 23:10:53 -0300

    Adiciona readAllMonths ao IOModule
    Função que le os meses escolhidos e retorna um vetor com os gastos e
    vendas

--- IOModule.lua
+++ IOModule.lua
@@ -2,31 +2,46 @@
 -- Modulo de leitura e escrita do programa
 
 -- Le os valores de um mes e retorna a soma dos gastos
---   e das vendas 
+--  e das vendas 
 function  readMonth(fileName)
 	local vendas, gastos = 0, 0
 
 
-	file = io.open(fileName, "r")
+	local file = io.open(fileName, "r")
 
 	-- A primeira linha do arquivo são os gastos
-	gastos = io.read(fileName)
+	gastos = file:read()
 
 	-- Verifica se o arquivo é valido
 	if not gastos then
-		io.close(fileName)
+		file:close()
 		return -1 
 	end
 
 	while true do
-		local venda = io.read(fileName)
+		local venda = file:read()
 
 		if not venda then break end
 		vendas = venda + vendas
 	end
 
 
-	io.close(fileName)
+	file:close()
+
+	return gastos, vendas
+end
+
+
+-- Usa a readMonth para ler todos os meses de "mes i.txt" até 
+--  "mes j.txt", armazena os gastos e vendas em um vetor
+--  e os retorna 
+function readAllMonths(i, j)
+	local mes 
+	local gastos, vendas = {}, {}
+
+	for mes = i,j do
+		gastos[mes], vendas[mes] = readMonth("mes " .. mes .. ".txt")
+	end
 
 	return gastos, vendas
 end
