34eeef41325be5c24184ee6f66d5c95464385135
Gabriel Rossi <rrossigabriel@gmail.com>
2016-04-14 00:14:58 -0300

    Adiciona comentarios

--- main.lua
+++ main.lua
@@ -9,18 +9,25 @@ require "IOModule"
 require "GrowthFunction"
 
 
+-- Recebe os valos do usuario
+-- sobre os meses a serem analisados
 print("Calculo de Crescimento")
 print("Digite o mes inicial e final para a analise:")
 local mesInicial, mesFinal = io.read("*n"), io.read("*n")
 
+-- Le os meses dos arquivos e armazena em vetores
 local custos, vendas = readAllMonths(mesInicial, mesFinal)
 
+-- Calcula o crescimento total liquido e bruto
 local crescimentoBruto, crescimentoLiquido = calculateTotalGrowth(custos, vendas)
 
+
+-- Calcula o crescimento mensal liquido e bruto
 local crescimentoLiquidoMes, crescimentoBrutoMes = {}, {}
 
 local crescimentoLiquidoMes, crescimentoBrutoMes = calculateMensalGrowth(custos, vendas)
 
+-- Escreve na tela as informações calculadas
 print("Crescimento Total Liquido: " .. string.format("%.2f",crescimentoLiquido*100).. "%")
 print("Crescimento Total Bruto: " .. string.format("%.2f",crescimentoBruto*100) .. "%")
 
